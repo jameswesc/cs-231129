c06db29e9330164bef6260c2dd95851a3cef8b51
James Wesc <jms.wsc@gmail.com>
2023-11-30 13:21:28 +1100

    seo tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,15 @@
 	import Scene from './Scene.svelte'
 </script>
 
+<svelte:head>
+	<title>CS231129 by James Wesc</title>
+	<meta
+		name="description"
+		content="Code sketch for 29 Nov 2023. Recreating a futuristic UI element from Rogue One with Threlte (Svelte + Three.js)"
+	/>
+	<meta name="author" content="James Wesc" />
+</svelte:head>
+
 <div class="fixed w-full h-full top-0 left-0">
 	<Canvas>
 		<Scene />
